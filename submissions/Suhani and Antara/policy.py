6644f9711cf2e7267afaceed4c26983ec0dd769d
Suhani Joshi <70758288+Suhani0411@users.noreply.github.com>
2026-07-09 13:45:33 +0530

    Update policy.py

--- submissions/Suhani and Antara/policy.py
+++ submissions/Suhani and Antara/policy.py
@@ -1,30 +1,24 @@
 import numpy as np
-
 class Policy:
     def __init__(self, n_arms, horizon):
         self.n_arms = n_arms
         self.horizon = horizon
-
-        self.alpha = np.ones(n_arms)
-        self.beta = np.ones(n_arms)
-
+        self.alpha = np.full(n_arms, 0.5)
+        self.beta = np.full(n_arms, 0.5)
         self.t = 0
+        self.min_rounds = 5   # show every ad at least 5x before trusting comparisons
 
     def select_arm(self):
-
-        if self.t < self.n_arms:
-            return self.t
-
-        progress = (self.t / self.horizon) ** 1.5
+        if self.t < self.n_arms * self.min_rounds:
+            return self.t % self.n_arms
+        progress = (self.t / self.horizon) ** 0.2
         samples = np.random.beta(self.alpha, self.beta)
-        means = (self.alpha - 1) / (self.alpha + self.beta - 2 + 1e-9)
-        score = (1-progress)*samples + progress*means
+        means = self.alpha / (self.alpha + self.beta)
+        score = (1 - progress) * samples + progress * means
         return int(np.argmax(score))
 
     def update(self, arm, reward):
-
         self.t += 1
-
         if reward > 0:
             self.alpha[arm] += 1
         else:
